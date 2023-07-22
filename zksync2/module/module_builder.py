cc5d49bee80eeff5866d7d0ca20c214574855138
Nicholas Pipitone <npip99@gmail.com>
2023-07-22 11:45:14 -0400

    Allow session to be passed into the Provider

--- zksync2/module/module_builder.py
+++ zksync2/module/module_builder.py
@@ -6,13 +6,14 @@ from typing import Union
 from web3._utils.module import attach_modules
 from eth_typing import URI
 from web3 import Web3
+from typing import Any
 
 
 class ZkSyncBuilder:
     @classmethod
-    def build(cls, url: Union[URI, str]) -> Web3:
+    def build(cls, url: Union[URI, str], session: Any = None) -> Web3:
         web3_module = Web3()
-        zksync_provider = ZkSyncProvider(url)
+        zksync_provider = ZkSyncProvider(url, session=session)
         zksync_middleware = build_zksync_middleware(zksync_provider)
         web3_module.middleware_onion.add(zksync_middleware)
         attach_modules(web3_module, {"zksync": (ZkSync,)})
